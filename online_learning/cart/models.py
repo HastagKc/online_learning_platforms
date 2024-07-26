b94377c77b1b523f32a8e18c69c997c09ac64bcb
HastagKc <chaudharykshittiz950@gmail.com>
2024-07-26 21:24:43 +0545

    working on enrollement

--- online_learning/cart/models.py
+++ online_learning/cart/models.py
@@ -3,7 +3,6 @@ from accounts.models import CustomUserModel
 from online_learning_app.models import Course
 
 
-# model
 # cart model
 # Cart represents the overall course cart for a user.
 class Cart(models.Model):
@@ -25,3 +24,30 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f'{self.course.course_title} in cart {self.cart.id}'
+
+
+# payment model
+class Payment(models.Model):
+    cart_item = models.ForeignKey(
+        CartItem, on_delete=models.CASCADE, related_name='payments')
+    amount = models.FloatField()
+    payment_id = models.CharField(max_length=200)
+    student = models.ForeignKey(
+        CustomUserModel, on_delete=models.CASCADE, related_name='payments')
+    paid_at = models.DateTimeField()
+    # Add this field to link payment to course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Payment {self.payment_id} for {self.amount}'
+
+
+# enrollment model
+class Enrollment(models.Model):
+    payment = models.ForeignKey(
+        Payment, on_delete=models.CASCADE, related_name='enrollments')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enroll_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Enrollment in {self.course.course_title} at {self.enroll_at}'
