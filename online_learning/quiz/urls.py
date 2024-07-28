2135378c76db70bc72d2460c3a876c9626330d96
HastagKc <chaudharykshittiz950@gmail.com>
2024-07-28 19:37:14 +0545

    question is showing correctly

--- online_learning/quiz/urls.py
+++ online_learning/quiz/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('showQuiz/', showQuiz, name='showQuiz'),
+    path('showQuiz/<int:course_id>/', showQuiz, name='showQuiz'),
+    path('takeQuiz/<int:quiz_id>/', takeQuiz, name='takeQuiz'),
 
 ]
--- online_learning/quiz/views.py
+++ online_learning/quiz/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render,get_list_or_404
+from django.shortcuts import render, get_object_or_404
+from online_learning_app.models import Course
+from .models import *
 
 # Create your views here.
 
 # quiz
 
 
-def showQuiz(request):
-    return render(request, 'online_learning_app/quiz/quiz.html')
+def showQuiz(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    quizes = course.quizzes.all()
+    context = {
+        'quizes': quizes,
+    }
+
+    return render(request, 'quiz/quiz.html', context=context)
 
 
+def takeQuiz(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    questions = quiz.questions.all()
 
+    context = {
+        'questions': questions,
+    }
+
+    return render(request, 'quiz/take_quiz.html', context=context)
