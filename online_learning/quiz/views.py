f8771958facbe33b1ace39bcb84a16ddaf095efe
HastagKc <chaudharykshittiz950@gmail.com>
2024-08-01 23:49:42 +0545

    quiz is showing perfectly

--- online_learning/quiz/views.py
+++ online_learning/quiz/views.py
@@ -7,7 +7,9 @@ from online_learning_app.models import Course
 
 def show_quiz(request, course_id):
     course = get_object_or_404(Course, id=course_id)
+    quizes = course.quiz.all()
     context = {
         'course': course,
+        'quizes': quizes
     }
     return render(request, 'quiz/show_quiz.html', context=context)
