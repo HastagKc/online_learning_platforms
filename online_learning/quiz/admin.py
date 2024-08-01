fa78247a8955fd4fc04fdcb6da967b27f399bd8f
HastagKc <chaudharykshittiz950@gmail.com>
2024-08-01 23:37:26 +0545

    model is created

--- online_learning/quiz/admin.py
+++ online_learning/quiz/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Quiz, Question, Answer, UserQuizProgress, UserAnswer, Options
 
 # Register your models here.
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['id', 'course', 'quiz_title', 'quiz_desc']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'quiz', 'question_text']
+
+
+@admin.register(Options)
+class OptionsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'options']
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'answer', 'is_correct']
+
+
+@admin.register(UserQuizProgress)
+class UserQuizProgressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'quiz', 'score', 'completed']
+
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'question', 'answer', 'is_correct']
--- online_learning/quiz/models.py
+++ online_learning/quiz/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from online_learning_app.models import Course
+
+from accounts.models import CustomUserModel
 
 # Create your models here.
+# quiz model
+
+
+class Quiz(models.Model):
+    course = models.ForeignKey(
+        Course, related_name='quiz', on_delete=models.CASCADE
+    )
+    quiz_title = models.CharField(max_length=200)
+    quiz_desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.quiz_title
+
+
+# question model
+class Question(models.Model):
+    quiz = models.ForeignKey(
+        Quiz, related_name='questions', on_delete=models.CASCADE
+    )
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+# options model
+class Options(models.Model):
+    question = models.ForeignKey(
+        Question, related_name='options', on_delete=models.CASCADE
+    )
+    options = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.question} - {self.options}'
+
+
+# answer model
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question, related_name='answer', on_delete=models.CASCADE
+    )
+    answer = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer
+
+
+# progress model
+class UserQuizProgress(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.quiz.title} - {self.score}'
+
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.user.username} - {self.question.text} - {self.answer.text} - {"Correct" if self.is_correct else "Incorrect"}'
